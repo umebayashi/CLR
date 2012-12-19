d4701111403b8b238066bbc442d3462614f70c6d
akihiro takada <umebayashi.akihiro@gmail.com>
2012-12-19 18:30:32 +0900

    2012/12/19 18:30 修正分

---
+++ Umebayashi.Enterprise/SharePoint/WinRT/Umebayashi.Enterprise.SharePoint.Library.Test/SharePoint/ListsManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Umebayashi.Enterprise.SharePoint
+{
+	[TestClass]
+	public class ListsManagerTest
+	{
+		[TestMethod]
+		public void NewTest()
+		{
+			var manager = new ListsManager();
+		}
+
+		[TestMethod]
+		public void GetListCollectionTest()
+		{
+			var manager = new ListsManager();
+
+			manager.GetListCollection();
+		}
+	}
+}
