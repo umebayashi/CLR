38d19e6f1b5853544658e05b7bab40de6385904c
akihiro takada <umebayashi.akihiro@gmail.com>
2013-01-21 18:18:53 +0900

    2013/01/21 18:20 修正分

---
+++ Umebayashi.Enterprise/StoreApp/WinRT/Umebayashi.Enterprise.StoreApp.Library/StoreApp/UI/Validation/ValidationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace Umebayashi.Enterprise.StoreApp.UI.Validation
+{
+	public class ValidationManager : DependencyObject
+	{
+		public static readonly DependencyProperty ValidationRulesProperty = DependencyProperty.RegisterAttached(
+			"ValidationRules",
+			typeof(ValidationRuleCollection),
+			typeof(ValidationManager),
+			new PropertyMetadata(null, null));
+
+		public static ValidationRuleCollection GetValidationRules(DependencyObject target)
+		{
+			return (ValidationRuleCollection)target.GetValue(ValidationRulesProperty);
+		}
+
+		public static void SetValidationRules(DependencyObject target, ValidationRuleCollection value)
+		{
+			target.SetValue(ValidationRulesProperty, value);
+		}
+	}
+}
---
+++ Umebayashi.Enterprise/StoreApp/WinRT/Umebayashi.Enterprise.StoreApp.Library/StoreApp/UI/Validation/ValidationRule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace Umebayashi.Enterprise.StoreApp.UI.Validation
+{
+	public abstract class ValidationRule : DependencyObject
+	{
+	}
+
+	public class CompareValidationRule : ValidationRule
+	{
+	}
+
+	public class CustomValidationRule : ValidationRule
+	{
+	}
+
+	public class RangeValidationRule : ValidationRule
+	{
+		#region field / property
+
+		public static readonly DependencyProperty MinProperty = DependencyProperty.Register(
+			"Min",
+			typeof(string),
+			typeof(RangeValidationRule),
+			new PropertyMetadata(null, null));
+
+		public string Min
+		{
+			get { return (string)this.GetValue(MinProperty); }
+			set { this.SetValue(MinProperty, value); }
+		}
+
+		public static readonly DependencyProperty MaxProperty = DependencyProperty.Register(
+			"Max",
+			typeof(string),
+			typeof(RangeValidationRule),
+			new PropertyMetadata(null, null));
+
+		public string Max
+		{
+			get { return (string)this.GetValue(MaxProperty); }
+			set { this.SetValue(MaxProperty, value); }
+		}
+
+		public static readonly DependencyProperty DataTypeProperty = DependencyProperty.Register(
+			"DataType",
+			typeof(ValidationDataType),
+			typeof(RangeValidationRule),
+			new PropertyMetadata(null, null));
+
+		public ValidationDataType DataType
+		{
+			get { return (ValidationDataType)this.GetValue(DataTypeProperty); }
+			set { this.SetValue(DataTypeProperty, value); }
+		}
+
+		#endregion
+	}
+
+	public class RegularExpressionValidationRule : ValidationRule
+	{
+	}
+
+	public class RequiredValidationRule : ValidationRule
+	{
+	}
+
+	public enum ValidationDataType
+	{
+		String,
+		Int32,
+		Double,
+		DateTime
+	}
+}
